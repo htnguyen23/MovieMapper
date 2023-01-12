91ca7fecf572b820347cb5267d72cdc3d45a04ff
Huong Nguyen <79285136+htnguyen23@users.noreply.github.com>
2023-01-11 23:31:21 -0600

    Create BackendInterface.java

---
+++ BackendInterface.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+public interface BackendInterface {
+        public void addGenre(String genre);
+        public void addAvgRating(String rating);
+        public void removeGenre(String genre);
+        public void removeAvgRating(String rating);
+        public List<String> getGenres();
+        public List<String> getAvgRatings();
+        public int getNumberOfMovies();
+        public List<String> getAllGenres();
+        public List<MovieInterface> getThreeMovies(int startingIndex);
+        //public ArrayList<Movie> getMovieSet(); //JUST TO TEST - REMOVE WHEN DONE
+}
